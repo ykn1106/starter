9b65e2edf803bf2889814092527f8c71b1fdd209
ykn <ykn1106@gmail.com>
2018-10-11 17:56:56 +0800

    添加logger显示页面

--- starter/views.py
+++ starter/views.py
@@ -5,6 +5,12 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from starter import app
+import os
+
+# __file__ refers to the file settings.py 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
+APP_DATA = os.path.join(APP_ROOT, 'data')
+
 
 @app.route('/')
 @app.route('/home')
@@ -15,3 +21,35 @@ def home():
         title='Home Page',
         year=datetime.now().year,
     )
+
+@app.route('/logger')
+def logger():
+    """Renders the logger page."""
+    filepath = 'log/log.txt'
+    return render_template(
+        'logger.html',
+        title='Logger Page',
+        date= datetime.now().strftime("%Y-%m-%d %a"),
+        content = readFile(filepath)
+    )
+
+def readFile(filepath):
+    content = ""
+    try:
+        with open(os.path.join(APP_DATA, filepath)) as f:
+            content=f.read()
+    except FileNotFoundError as e:
+        errorMsg = "文件不存在,请检查文件路径"
+        content = "Error：" + errorMsg
+        print(content)
+    except MemoryError as e:
+        errorMsg = "内存错误"
+        content = "Error：" + errorMsg
+        print(content)
+    except SyntaxError as e:
+        errorMsg = "系统错误"
+        content = "Error：" + errorMsg
+        print(content)
+    finally:
+        print("操作完毕.")
+        return content
